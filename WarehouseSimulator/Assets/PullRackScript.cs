b7c180ff2d326c1a16d4f9bb3f234de9703ecfe9
Mack Fischer <mackfisch@gmail.com>
2024-06-30 12:00:31 -0600

    Adjustments to pull rack script. Slightly easier to place items, but abandoned grid snapping.

--- WarehouseSimulator/Assets/PullRackScript.cs
+++ WarehouseSimulator/Assets/PullRackScript.cs
@@ -10,7 +10,7 @@ public class PullRackScript : MonoBehaviour
     Collider rackCollider;
     void Start()
     {
-        
+        rackCollider = GetComponent<Collider>();
     }
 
     // Update is called once per frame
@@ -23,7 +23,20 @@ public class PullRackScript : MonoBehaviour
     {
         if (other.CompareTag("canPickUp"))
         {
-            other.transform.position = this.transform.position;
+            var otherRb = other.GetComponent<Rigidbody>();
+            otherRb.velocity = Vector3.zero;
+            otherRb.freezeRotation = true;
+            otherRb.rotation = Quaternion.identity;
+            /*var gridY = other.transform.position.y - (rackCollider.transform.position.y - rackCollider.bounds.size.y / 2);
+            var gridZ = other.transform.position.z - (rackCollider.transform.position.z - rackCollider.bounds.size.z / 2);
+            var newY = other.transform.position.y - (mod(gridY, rackCollider.bounds.size.y / 3)) + (rackCollider.bounds.size.y / 3) * 0.5f;
+            var newZ = other.transform.position.z - (mod(gridZ, rackCollider.bounds.size.z / 3)) + (rackCollider.bounds.size.z / 3) * 0.5f;*/
+            other.transform.position = new Vector3(this.transform.position.x, other.transform.position.y, other.transform.position.z);
         }
+    }
+
+    float mod(float x, float m)
+    {
+        return (x % m + m) % m;
     }
 }
